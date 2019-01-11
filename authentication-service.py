22fe3ef6d26bf361b71c51c3cafd2d89c4a35dba
mohammed-ali-1 <mohammad10596@gmail.com>
2019-01-11 14:23:23 +0100

    consolidated into 1 file

--- authentication-service.py
+++ authentication-service.py
@@ -3,17 +3,6 @@ import csv
 import os.path as path
 import getpass
 
-def get_user_creds():
-    user_name = input("Your user name: ")
-    user_pass = getpass.getpass().encode()
-    return user_name, user_pass
-
-def hash_pass(pword):
-    salt = bcrypt.gensalt()
-    hashed_pass = bcrypt.hashpw(user_pass, salt)
-    hashed_pass = hashed_pass.decode()
-    return hashed_pass
-
 def unique(uname):
     with open("user_store.store") as user_store:
         reader = csv.reader(user_store, delimiter=",")
@@ -42,7 +31,34 @@ def add_user_to_store(uname, hashed_pword):
         return
     else:
         print("User already exists")
+        return
 
-user_name, user_pass = get_user_creds()
-hashed_pword = hash_pass(user_pass)
-add_user_to_store(user_name, hashed_pword)
+def validate_login(uname, pword):
+    hashed_pword = get_hash_from_store(uname)
+    return True if hashed_pword == bcrypt.hashpw(pword.encode(), hashed_pword) else False
+
+def get_hash_from_store(uname):
+    with open("user_store.store") as user_store:
+        reader = csv.reader(user_store, delimiter=",")
+        line_count = 0
+        for line in reader:
+            if line_count == 0:
+                line_count += 1
+                continue
+            else:
+                user_name_from_store = line[0]
+                hash_from_store = line[1]
+                if user_name_from_store == uname:
+                   return hash_from_store.encode()
+                line_count += 1
+
+def get_user_creds():
+    user_name = input("Your username: ")
+    user_pass = getpass.getpass().encode()
+    return user_name, user_pass
+
+def hash_pass(pword):
+    salt = bcrypt.gensalt()
+    hashed_pass = bcrypt.hashpw(pword, salt)
+    hashed_pass = hashed_pass.decode()
+    return hashed_pass
