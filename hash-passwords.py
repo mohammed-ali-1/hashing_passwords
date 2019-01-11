9042662222e304acdb7f94e18f48684a30368ea7
mohammed-ali-1 <mohammad10596@gmail.com>
2019-01-11 13:50:10 +0100

    made sure username is unique

--- hash-passwords.py
+++ hash-passwords.py
@@ -1,9 +1,11 @@
 import bcrypt
+import csv
 import os.path as path
+import getpass
 
 def get_user_creds():
     user_name = input("Your user name: ")
-    user_pass = input("Your password: ").encode()
+    user_pass = getpass.getpass().encode()
     return user_name, user_pass
 
 def hash_pass(pword):
@@ -12,14 +14,34 @@ def hash_pass(pword):
     hashed_pass = hashed_pass.decode()
     return hashed_pass
 
+def unique(uname):
+    with open("user_store.store") as user_store:
+        reader = csv.reader(user_store, delimiter=",")
+        line_count = 0
+        for line in reader:
+            if line_count == 0:
+                line_count += 1
+                continue
+            else:
+                user_name_from_store = line[0]
+                if user_name_from_store == uname:
+                   return False
+                line_count += 1
+    return True
+
 def add_user_to_store(uname, hashed_pword):
     if not path.exists("user_store.store"):
         with open("user_store.store", "a+") as f:
             f.write("uname,hashed_pword\n")
-    with open("user_store.store", "a+") as f:
-        f.write(f"{uname},{hashed_pword}\n")
-
-    print("User successfully added")
+            f.write(f"{uname},{hashed_pword}\n")
+            print ("User successfully added")
+    if unique(uname):
+        with open("user_store.store", "a+") as f:
+            f.write(f"{uname},{hashed_pword}\n")
+        print("User successfully added")
+        return
+    else:
+        print("User already exists")
 
 user_name, user_pass = get_user_creds()
 hashed_pword = hash_pass(user_pass)
